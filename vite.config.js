4c29c084e9bf546ef44ca2a985fddd4685e22577
Priyanshi Kothari <priyanshikothari522@gmail.com>
2026-04-02 01:44:22 +0530

    Add base URL configuration to Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/",   // 🔥 ADD THIS LINE
   server: {
     port: 5173,
     strictPort: true,
@@ -14,4 +15,4 @@ export default defineConfig({
     strictPort: true,
     host: true,
   },
-});
+})
